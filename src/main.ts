79c6e73924cb2b53b106e875d5df65fc4c90db3f
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2019-03-07 18:36:08 +0100

    feat: integrate swagger for api docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
@@ -7,10 +8,21 @@ import { validationExceptionFactory } from './common/factories/validation-except
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // use morgan
   app.use(morgan('dev'));
+  // register validation pipe with custom error formatting factory
   app.useGlobalPipes(new ValidationPipe({
     exceptionFactory: validationExceptionFactory,
   }));
+  // swagger options
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('V2X Network Core API')
+    .setDescription('REST API documentation for V2X Network Core Engine')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
   await app.listen(3000);
 }
 bootstrap();
