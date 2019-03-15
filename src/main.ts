f03b609e40e7e8856b9e86907903dab89b0f497d
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2019-03-15 12:19:59 +0100

    config: use port specified in .env for running application

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,22 @@ import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
 import { validationExceptionFactory } from './common/factories/validation-exception.factory';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // config service instance
+  const configService: ConfigService = app.get(ConfigService);
+
   // use morgan
   app.use(morgan('dev'));
+
   // register validation pipe with custom error formatting factory
   app.useGlobalPipes(new ValidationPipe({
     exceptionFactory: validationExceptionFactory,
   }));
+
   // swagger options
   const swaggerOptions = new DocumentBuilder()
     .setTitle('V2X Network Core API')
@@ -23,6 +30,9 @@ async function bootstrap() {
     .build();
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('api-docs', app, swaggerDocument);
-  await app.listen(3000);
+
+  // start the app
+  await app.listen(configService.appPort);
 }
+
 bootstrap();
